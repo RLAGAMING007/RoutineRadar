6c6bbf864aa5f2e0146c9e14805bbf6886257401
RLAGAMING007 <k.l.kumar006@gmail.com>
2024-02-03 08:54:24 +0530

    fixed checkbox

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -7,7 +7,13 @@ const AddTask = (props) => {
         <div className="task-container">
           <div className="add-tasks">
             <div class="custom-checkbox">
-              <input type="checkbox" />
+              <input type="checkbox" id="input-1" className="check-input" />
+
+              <label htmlFor="input-1" className="checkbox">
+                <svg viewBox="0 0 22 16" fill="none">
+                  <path d="M1 6.85L8.09677 14L21 1" />
+                </svg>
+              </label>
             </div>
             <div className="task">
               <h2>{props.title}</h2>
